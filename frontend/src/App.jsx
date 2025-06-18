1e53f41834ce8c2959b93a51187c2836fbb91d21
miglios2912 <simone.miglio@tum.de>
2025-06-18 14:51:26 +0200

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'; // or ChatWindow.css
+import ChatWindow from './components/ChatWindow';
+
+export default function App() {
+  return (
+    <div className="chat-wrapper">
+      <header className="chat-header">
+        <img src="/tum-logo.png" alt="TUM Logo" className="chat-logo" />
+      </header>
+
+      <main>
+        <div className="chat-container">
+          <ChatWindow />
+        </div>
+      </main>
+    </div>
+  );
+}
